703315a53b39b8cc7535448f8ff7236995692550
Kyle Jordan <kjord021@fiu.edu>
2021-01-16 12:24:09 -0500

    Add project files.

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Commander.Data;
+using Commander.Dtos;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands() 
+        {
+            var commandItems = _repository.GetAllCommands();
+
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult <CommandReadDto> GetCommandById(int id) 
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            if (commandItem != null) 
+            {
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,33 @@
+﻿
+using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+
+        //use this until you get your server up and running
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil Water", Platform = "Kettle & Pan" },
+                new Command { Id = 0, HowTo = "Cut Bread", Line = "Get a knife", Platform = "Knife and chopping board" },
+                new Command { Id = 0, HowTo = "Make cup of tea", Line = "Place teabag in cup", Platform = "Kettle & Cup" },
+        };
+
+            return commands;
+
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil Water", Platform = "Kettle & Pan" };
+        }
+    }
+}
